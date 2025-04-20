4306b7f0e90da61b561c8d94fbc9921815f0af78
dev_in_loop <venkateshkilari2@gmail.com>
2025-04-20 22:24:47 +0530

    Update main.tf

--- 70-load-balancer/main.tf
+++ 70-load-balancer/main.tf
@@ -30,8 +30,8 @@ resource "aws_lb" "app_lb" {
 }
 
 # Target group
-resource "aws_lb_target_group" "app_tg" {
-  name     = "my-app-tg"
+resource "aws_lb_target_group" "app_tg_blue" {
+  name     = "my-app-tg-blue"
   port     = 80
   protocol = "HTTP"
   vpc_id   = data.terraform_remote_state._10-vpc.outputs.vpc_id
@@ -43,6 +43,26 @@ resource "aws_lb_target_group" "app_tg" {
     unhealthy_threshold = 2
   }
  
+   lifecycle {
+     create_before_destroy = true
+   }
+}
+
+resource "aws_lb_target_group" "app_tg_green" {
+  name     = "my-app-tg-green"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = data.terraform_remote_state._10-vpc.outputs.vpc_id
+  health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold  = 2
+    unhealthy_threshold = 2
+  }
+  lifecycle {
+     create_before_destroy = true
+   }
 }
 
 # Listener
@@ -53,14 +73,23 @@ resource "aws_lb_listener" "app_listener" {
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.app_tg.arn
+    #target_group_arn = aws_lb_target_group.app_tg_blue.arn
+    target_group_arn = aws_lb_target_group.app_tg_green.arn
   }
-
 
 }
 
-resource "aws_lb_target_group_attachment" "ec2_attach" {
-  target_group_arn = aws_lb_target_group.app_tg.arn
+resource "aws_lb_target_group_attachment" "ec2_blue_attach" {
+  target_group_arn = aws_lb_target_group.app_tg_blue.arn
   target_id        = data.terraform_remote_state._60-virtual-machine.outputs.blue_instance_id
   port             = 80
+
+  }
+
+  
+resource "aws_lb_target_group_attachment" "ec2_green_attach" {
+  target_group_arn = aws_lb_target_group.app_tg_green.arn
+  target_id        = data.terraform_remote_state._60-virtual-machine.outputs.green_instance_id
+  port             = 80
+
   }
